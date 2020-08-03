a7613f5bec6df722514ca46bc72b8af306727a46
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-08-02 18:55:08 -0700

    props

--- src/index.js
+++ src/index.js
@@ -3,28 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.css';
 
 // useState() is a hook, so that it hooks this symbl to this state.
-function Button() {
-    const [counter, setCounter] = useState(5);
-    const handleClick = () => setCounter(counter + 1);
-    return (<button onClick={handleClick}>
-        {counter}
-    </button>
+function Button(props) {
+    // const handleClick = () => setCounter(counter + 1);
+    return (
+        <button onClick={props.onClickFunction}>
+            +1
+        </button>
     );
 }
 
 // Display (does not have state of it's own)
-function Display(){
+function Display(props) {
     return (
-        <div>...</div>
+        <div>{props.message}</div>
     );
 }
 
 // App
-function App(){
+function App() {
+    const [counter, setCounter] = useState(5);
+    const incrementCounter = () => setCounter(counter +1);
     return (
         <React.Fragment>
-            <Button />
-            <Display />
+            <Button onClickFunction={incrementCounter}/>
+            <Display message={counter}/>
         </React.Fragment>
     );
 }
